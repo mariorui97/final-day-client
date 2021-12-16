bb85a5eba5abe60d37bf099ee34f578623004ce0
Mário Rui <marinhocosta@live.com.pt>
2021-12-16 18:12:58 +0000

    rank icons

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_URL = 'http://localhost:5005/api'
 const SOCKET_URL = 'http://localhost:5005/'
-//const SOCKET_URL = 'https://duo-diff.herokuapp.com/
+//const SOCKET_URL = 'https://duo-diff.herokuapp.com/'
 //const API_URL = 'https://duo-diff.herokuapp.com/api'
 export {API_URL}
 export {SOCKET_URL}
